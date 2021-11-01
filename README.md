3dd14ac9e6357d75037740ba7db9f5b42c9e1069
Subramanya K S <subramanyaks2205@gmail.com>
2021-11-01 11:47:46 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Weather App using openweathermap API
+
+This is my hobby project used to learn how to use public api for project.In this i used the api of openweathermap to featch the weather details of various city across the globe by giving city name as input.
+
+#### Technologies used:
+* Python
+* Django
+* openweathermap api
+* HTML
+
+### References:
+&hearts; Thanks to
+https://openweathermap.org/api
+https://youtu.be/E-n5QAuQSr8
+
---
+++ weatherapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+# Create your views here.
+def index(request):
+    if request.method =="POST":
+        city=request.POST['city']
+        source=urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +city + '&units=metric&appid=619d57c0f016f91a8aa128eb3155002a').read()
+        list_of_data=json.loads(source)
+        data={
+            "country_code":str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ', '+ str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['main']['temp']) + ' °C',
+            "pressure": str(list_of_data['main']['pressure']),
+            "humidity": str(list_of_data['main']['humidity']),
+            'main': str(list_of_data['weather'][0]['main']),
+            'description': str(list_of_data['weather'][0]['description']),
+            'icon': list_of_data['weather'][0]['icon'],
+
+        }
+        print(data)
+    else:
+        data={}
+    return render(request,"index.html",data)
